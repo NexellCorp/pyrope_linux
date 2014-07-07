126953eff585703bd2daf15bddb3d214b3bb15e5
SeongO Park <ray@nexell.co.kr>
2014-07-07 18:59:21 +0900

    [ISO7816]  * Distingush UART H/W ch & Logical ch to configuration.
    Former-commit-id: c3e4e5d9cadecfb584e25bedd049704e296322eb

--- library/src/libnxiso7816/iso7816_cfg.h
+++ library/src/libnxiso7816/iso7816_cfg.h
@@ -7,7 +7,8 @@
 //#define	USE_EXTERNAL_CLK
 #define	RST_GPIO_PORT		GPIOC4
 #define	CLK_PWM_PORT		PWM_1
-#define	DATA_UART_PORT		4		//	0,1,2,3,4,5
+#define	DATA_UART_PORT		4		//	ttyAMA Port Number( Logical Port )
+#define	HW_UART_PORT		4		//	H/W UART Port Number ( Physical Port )
 
 
 #endif	//	__ISO7816_CFG_H__
--- library/src/libnxmalloc/Android.mk
+++ library/src/libnxmalloc/Android.mk
@@ -3,18 +3,15 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 # LOCAL_PRELINK_MODULE := false
 
-NX_HW_TOP := $(TOP)/hardware/nexell/pyrope/
-NX_HW_INCLUDE := $(NX_HW_TOP)/include
-
 LOCAL_SHARED_LIBRARIES :=	\
 	liblog \
 	libcutils
 
-LOCAL_C_INCLUDES := system/core/include/ion \
-					$(NX_HW_TOP)/include \
+LOCAL_C_INCLUDES += system/core/include/ion \
+					$(TOP)/hardware/nexell/pyrope/include \
 					$(TOP)/linux/pyrope/library/include
 
-LOCAL_CFLAGS := 
+LOCAL_CFLAGS += 
 
 LOCAL_SRC_FILES := \
 	nx_alloc_mem_ion.c

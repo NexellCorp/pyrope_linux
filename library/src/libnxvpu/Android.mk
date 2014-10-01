471b9ac8358d21a24bf77ee05864063e07471883
KOO Bon-Gyu <freestyle@nexell.co.kr>
2014-10-01 11:28:09 +0900

    [FIX] nx_vpu build break
    Signed-off-by: KOO Bon-Gyu <freestyle@nexell.co.kr>
    
    Former-commit-id: 73a596eb8df4cc0e83bb755efd449e76aafa331d

--- library/src/libnxvpu/Android.mk
+++ library/src/libnxvpu/Android.mk
@@ -6,6 +6,8 @@ include $(CLEAR_VARS)
 NX_PYROPE_INCLUDE := $(TOP)/hardware/nexell/pyrope/include
 NX_LINUX_INCLUDE  := $(TOP)/linux/pyrope/library/include
 
+RATECONTROL_PATH := $(TOP)/linux/pyrope/library/lib/ratecontrol
+
 LOCAL_SHARED_LIBRARIES :=	\
 	liblog \
 	libcutils \
@@ -13,8 +15,7 @@ LOCAL_SHARED_LIBRARIES :=	\
 	libion-nexell
 
 LOCAL_STATIC_LIBRARIES := \
-	libnxmalloc \
-	libnxvidrc_android
+	libnxmalloc
 
 LOCAL_C_INCLUDES := system/core/include/ion \
 					$(NX_PYROPE_INCLUDE) \
@@ -24,6 +25,10 @@ LOCAL_CFLAGS :=
 
 LOCAL_SRC_FILES := \
 	nx_video_api.c
+
+LOCAL_LDFLAGS += \
+	-L$(RATECONTROL_PATH)	\
+	-lnxvidrc_android
 
 LOCAL_MODULE := libnx_vpu
 

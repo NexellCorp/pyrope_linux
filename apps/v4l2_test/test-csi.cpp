da0bb0d403d832c6aaef25986295df18ff13c2eb
Sungwoo Park <swpark@nexell.co.kr>
2014-11-28 15:17:51 +0900

    [FIX] test-csi: add module argument
    Former-commit-id: d61ae03ceb8f59d334e18c5827486df4756fe1bb

--- apps/v4l2_test/test-csi.cpp
+++ apps/v4l2_test/test-csi.cpp
@@ -121,13 +121,17 @@ int alloc_buffers(int ion_fd, int count, struct nxp_vid_buffer *bufs, int width,
 
 #define V4L2_CID_CAMERA_MODE_CHANGE     (V4L2_CTRL_CLASS_CAMERA | 0x1003)
 
-// # ./test w h
+// # ./test module w h
 int main(int argc, char *argv[])
 {
     int ion_fd = ion_open();
     int width;
     int height;
+    int clipper_id = nxp_v4l2_clipper0;
+    int sensor_id = nxp_v4l2_sensor0;
+    int video_id = nxp_v4l2_mlc0_video;
     int format = V4L2_PIX_FMT_YUV420M; // yuv420
+    int module;
     // int format = V4L2_PIX_FMT_YUV422P; // yuv422
 
     if (ion_fd < 0) {
@@ -135,97 +139,95 @@ int main(int argc, char *argv[])
         return -EINVAL;
     }
 
+
+    if (argc >= 4) {
+        module = atoi(argv[1]);
+        width = atoi(argv[2]);
+        height = atoi(argv[3]);
+    } else {
+        printf("usage: ./test module width height\n");
+        return 0;
+    }
+
     struct V4l2UsageScheme s;
     memset(&s, 0, sizeof(s));
 
-    s.useClipper1 = true;
-    s.useDecimator1 = true;
-    s.useMlc0Video = true;
-    // s.useMlc1Video = true;
-
-    if (argc >= 3) {
-        width = atoi(argv[1]);
-        height = atoi(argv[2]);
+    if (module == 0) {
+        s.useClipper0 = true;
+        s.useDecimator0 = true;
+        clipper_id = nxp_v4l2_clipper0;
+        sensor_id = nxp_v4l2_sensor0;
     } else {
-        width = DEFAULT_WIDTH;
-        height = DEFAULT_HEIGHT;
+        s.useClipper1 = true;
+        s.useDecimator1 = true;
+        clipper_id = nxp_v4l2_clipper1;
+        sensor_id = nxp_v4l2_sensor1;
     }
-    printf("width %d, height %d\n", width, height);
+    s.useMlc0Video = true;
+
+    printf("width %d, height %d, module %d\n", width, height, module);
 
     CHECK_COMMAND(v4l2_init(&s));
 
-    CHECK_COMMAND(v4l2_set_format(nxp_v4l2_clipper1, width, height, format));
-    CHECK_COMMAND(v4l2_set_crop(nxp_v4l2_clipper1, 0, 0, width, height));
-    CHECK_COMMAND(v4l2_set_format(nxp_v4l2_sensor1, width, height, V4L2_MBUS_FMT_YUYV8_2X8));
+    CHECK_COMMAND(v4l2_set_format(clipper_id, width, height, format));
+    CHECK_COMMAND(v4l2_set_crop(clipper_id, 0, 0, width, height));
+    CHECK_COMMAND(v4l2_set_format(sensor_id, width, height, V4L2_MBUS_FMT_YUYV8_2X8));
     CHECK_COMMAND(v4l2_set_format(nxp_v4l2_mipicsi, width, height, V4L2_MBUS_FMT_YUYV8_2X8));
-    CHECK_COMMAND(v4l2_set_format(nxp_v4l2_mlc0_video, width, height, format));
+    CHECK_COMMAND(v4l2_set_format(video_id, width, height, format));
 
-    if (width > 1280 || height > 800)
-        CHECK_COMMAND(v4l2_set_crop(nxp_v4l2_mlc0_video, 0, 0, 1280, 800));
-    else
-        CHECK_COMMAND(v4l2_set_crop(nxp_v4l2_mlc0_video, 0, 0, width, height));
+    CHECK_COMMAND(v4l2_set_crop(video_id, 0, 0, width, height));
 
-    CHECK_COMMAND(v4l2_set_ctrl(nxp_v4l2_mlc0_video, V4L2_CID_MLC_VID_PRIORITY, 0));
-    CHECK_COMMAND(v4l2_set_ctrl(nxp_v4l2_mlc0_video, V4L2_CID_MLC_VID_COLORKEY, 0x0));
+    CHECK_COMMAND(v4l2_set_ctrl(video_id, V4L2_CID_MLC_VID_PRIORITY, 0));
+    CHECK_COMMAND(v4l2_set_ctrl(video_id, V4L2_CID_MLC_VID_COLORKEY, 0x0));
 
-    CHECK_COMMAND(v4l2_reqbuf(nxp_v4l2_clipper1, MAX_BUFFER_COUNT));
-    CHECK_COMMAND(v4l2_reqbuf(nxp_v4l2_mlc0_video, MAX_BUFFER_COUNT));
+    CHECK_COMMAND(v4l2_reqbuf(clipper_id, MAX_BUFFER_COUNT));
+    CHECK_COMMAND(v4l2_reqbuf(video_id, MAX_BUFFER_COUNT));
 
-    printf("alloc video\n");
     struct nxp_vid_buffer bufs[MAX_BUFFER_COUNT];
     CHECK_COMMAND(alloc_buffers(ion_fd, MAX_BUFFER_COUNT, bufs, width, height, format));
-    // CHECK_COMMAND(alloc_buffers(ion_fd, MAX_BUFFER_COUNT, bufs, width, height, V4L2_PIX_FMT_YUV444));
-    // HACK allocate width * 2
-    // CHECK_COMMAND(alloc_buffers(ion_fd, MAX_BUFFER_COUNT, bufs, width * 2, height, V4L2_PIX_FMT_YUV444));
-    printf("vid_buf: %p, %p, %p, %p\n", bufs[0].virt[0], bufs[1].virt[0], bufs[2].virt[0], bufs[3].virt[0]);
 
     int i;
     for (i = 0; i < MAX_BUFFER_COUNT; i++) {
         struct nxp_vid_buffer *buf = &bufs[i];
-        printf("buf plane num: %d\n", buf->plane_num);
-        CHECK_COMMAND(v4l2_qbuf(nxp_v4l2_clipper1, buf->plane_num, i, buf, -1, NULL));
+        CHECK_COMMAND(v4l2_qbuf(clipper_id, buf->plane_num, i, buf, -1, NULL));
     }
 
-    CHECK_COMMAND(v4l2_streamon(nxp_v4l2_clipper1));
+    CHECK_COMMAND(v4l2_streamon(clipper_id));
 
     int out_index = 0;
     int out_dq_index = 0;
     int out_q_count = 0;
     bool started_out = false;
     int j;
-    unsigned short *prgb_data;
-    struct nxp_vid_buffer *rgb_buf;
     int capture_index = 0;
-    int count = 0;
-    if (argc >= 4)
-        count = atoi(argv[3]);
+    int count = 10000;
     while (count >= 0) {
         struct nxp_vid_buffer *buf = &bufs[capture_index];
-        CHECK_COMMAND(v4l2_dqbuf(nxp_v4l2_clipper1, buf->plane_num, &capture_index, NULL));
-        CHECK_COMMAND(v4l2_qbuf(nxp_v4l2_mlc0_video, buf->plane_num, out_index, buf, -1, NULL));
+        CHECK_COMMAND(v4l2_dqbuf(clipper_id, buf->plane_num, &capture_index, NULL));
+        CHECK_COMMAND(v4l2_qbuf(video_id, buf->plane_num, out_index, buf, -1, NULL));
 
         out_q_count++;
         out_index++;
         out_index %= MAX_BUFFER_COUNT;
 
         if (!started_out) {
-            CHECK_COMMAND(v4l2_streamon(nxp_v4l2_mlc0_video));
+            CHECK_COMMAND(v4l2_streamon(video_id));
             started_out = true;
         }
 
         if (out_q_count >= MAX_BUFFER_COUNT) {
-            CHECK_COMMAND(v4l2_dqbuf(nxp_v4l2_mlc0_video, buf->plane_num, &out_dq_index, NULL));
+            CHECK_COMMAND(v4l2_dqbuf(video_id, buf->plane_num, &out_dq_index, NULL));
             out_q_count--;
         }
 
-        CHECK_COMMAND(v4l2_qbuf(nxp_v4l2_clipper1, buf->plane_num, capture_index, buf, -1, NULL));
+        CHECK_COMMAND(v4l2_qbuf(clipper_id, buf->plane_num, capture_index, buf, -1, NULL));
         count--;
     }
 
-    CHECK_COMMAND(v4l2_streamoff(nxp_v4l2_mlc0_video));
-    CHECK_COMMAND(v4l2_streamoff(nxp_v4l2_clipper1));
+    CHECK_COMMAND(v4l2_streamoff(video_id));
+    CHECK_COMMAND(v4l2_streamoff(clipper_id));
 
-    // v4l2_exit();
+    v4l2_exit();
     close(ion_fd);
 
     return 0;

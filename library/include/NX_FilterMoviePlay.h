08113523705694e8506b145e4679735337a713ec
HyunChul Jun <hcjun@nexell.co.kr>
2014-12-05 14:56:59 +0900

    1. Modify NX_MPOpen
    Former-commit-id: e4b17396d4dbc4090f4d3dd2dc00b72be6611a49

--- library/include/NX_FilterMoviePlay.h
+++ library/include/NX_FilterMoviePlay.h
@@ -66,20 +66,32 @@ typedef struct _MEDIA_INFO{
 extern "C" {
 #endif	//	__cplusplus
 
-MP_RESULT NX_MPCreate(MP_HANDLE *phandle, const char *uri, MEDIA_INFO *media_info,
-		void(*cb)(void *owner, unsigned int EventType, unsigned int EventDatam, unsigned int parm), void *cbPrivate);
-MP_RESULT NX_MPOpen(MP_HANDLE handle, int volumem, int dspModule, int dspPort, int audio_track_num, int video_track_num, int display);
+	MP_RESULT NX_MPSetFileName(MP_HANDLE *phandle, const char *uri, Media_Info *media_info);
+#ifdef ANDROID
+	MP_RESULT NX_MPOpen(MP_HANDLE handle, int32_t select_audio_track_num, int select_video_track_num, int pip,
+		void *pSurface_1, void *pSurface_2, void *pNull,
+		void(*cbEvent)(void *Obj, unsigned int EventType, unsigned int EventData, unsigned int parm), void *cbPrivate
+		);
+#else
+	MP_RESULT NX_MPOpen(MP_HANDLE handle, int audio_track_num, int video_track_num, int display,
+		void *volumem, void *dspModule, void *dspPort,
+		void(*cbEvent)(void *Obj, unsigned int EventType, unsigned int EventData, unsigned int parm), void *cbPrivate);
+#endif
 
-void NX_MPClose( MP_HANDLE handle );
-MP_RESULT NX_MPGetMediaInfo(MP_HANDLE handle, int index, MEDIA_INFO *pInfo);
-MP_RESULT NX_MPPlay( MP_HANDLE handle, float speed );
-MP_RESULT NX_MPPause(MP_HANDLE hande);
-MP_RESULT NX_MPStop(MP_HANDLE hande);
-MP_RESULT NX_MPSeek(MP_HANDLE hande, unsigned int seekTime);				//seekTime : msec
-MP_RESULT NX_MPGetCurDuration(MP_HANDLE handle, unsigned int *duration);	//duration : msec
-MP_RESULT NX_MPGetCurPosition(MP_HANDLE handle, unsigned int *position);	//position : msec	
-MP_RESULT NX_MPSetDspPosition(MP_HANDLE handle,	int dspModule, int dsPport, int x, int y, int width, int height );
-MP_RESULT NX_MPSetVolume(MP_HANDLE handle, int volume);						//volume range : 0 ~ 100
+	void NX_MPClose(MP_HANDLE handle);
+	MP_RESULT NX_MPGetMediaInfo(MP_HANDLE handle, int index, void *pInfo);
+	MP_RESULT NX_MPPlay(MP_HANDLE handle, float speed);
+	MP_RESULT NX_MPPause(MP_HANDLE hande);
+	MP_RESULT NX_MPStop(MP_HANDLE hande);
+	MP_RESULT NX_MPSeek(MP_HANDLE hande, unsigned int seekTime);				//seekTime : msec
+	MP_RESULT NX_MPGetCurDuration(MP_HANDLE handle, unsigned int *duration);	//duration : msec
+	MP_RESULT NX_MPGetCurPosition(MP_HANDLE handle, unsigned int *position);	//position : msec	
+#ifdef ANDROID
+
+#else
+	MP_RESULT NX_MPSetDspPosition(MP_HANDLE handle, int dspModule, int dsPport, int x, int y, int width, int height);
+	MP_RESULT NX_MPSetVolume(MP_HANDLE handle, int volume);						//volume range : 0 ~ 100
+#endif
 
 #ifdef __cplusplus
 }

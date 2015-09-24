969e328a37cf1c0b9949062130edae57fb606544
Bongkwan Kook <kook@nexell.co.kr>
2015-09-24 13:40:28 +0900

    [Release] - Ver 0.6.1
    Former-commit-id: eed9dbb7dba38008fa6336a184c58f5e6450f187

--- boot/release/2ndboot/copy2release_2ndboot.sh
+++ boot/release/2ndboot/copy2release_2ndboot.sh
@@ -4,24 +4,25 @@ SEC4418_NAME=S5P4418_2ndboot_DDR3_V061
 SEC4330_NAME=NXP4330_2ndboot_DDR3_V061
 
 DST4418_NAME1=2ndboot_svt
-DST4418_NAME2=2ndboot_dronel
-DST4418_NAME3=2ndboot_drone
+DST4418_NAME2=2ndboot_drone
+DST4418_NAME3=2ndboot_avn_ref
 
 DST4330_NAME1=2ndboot_lepus
 
 echo "$SECBOOT_NAME"
 
-cp ../../temporary/2ndboot/"$SEC4330_NAME"_SDMMC.bin ./"$DST4330_NAME1"_sdmmc.bin
-#cp ../../temporary/2ndboot/"$SEC4330_NAME"_USB.bin   ./"$DST4330_NAME1"_usb.bin
-
 cp ../../temporary/2ndboot/"$SEC4418_NAME"_SDMMC.bin ./"$DST4418_NAME1"_sdmmc.bin
 cp ../../temporary/2ndboot/"$SEC4418_NAME"_SDMMC.bin ./"$DST4418_NAME2"_sdmmc.bin
 cp ../../temporary/2ndboot/"$SEC4418_NAME"_SDMMC.bin ./"$DST4418_NAME3"_sdmmc.bin
 
-cp ../../temporary/2ndboot/"$SEC4418_NAME"_USB.bin ./"$DST4418_NAME1"_usb.bin
-cp ../../temporary/2ndboot/"$SEC4418_NAME"_USB.bin ./"$DST4418_NAME2"_usb.bin
-cp ../../temporary/2ndboot/"$SEC4418_NAME"_USB.bin ./"$DST4418_NAME3"_usb.bin
+cp ../../temporary/2ndboot/"$SEC4418_NAME"_USB.bin   ./"$DST4418_NAME1"_usb.bin
+cp ../../temporary/2ndboot/"$SEC4418_NAME"_USB.bin   ./"$DST4418_NAME2"_usb.bin
+cp ../../temporary/2ndboot/"$SEC4418_NAME"_USB.bin   ./"$DST4418_NAME3"_usb.bin
 
-cp ../../temporary/2ndboot/"$SEC4418_NAME"_SDFS.bin ./"$DST4418_NAME1"_sdfs.bin
-cp ../../temporary/2ndboot/"$SEC4418_NAME"_SPI.bin  ./"$DST4418_NAME1"_spi.bin
-cp ../../temporary/2ndboot/"$SEC4418_NAME"_NAND.bin ./"$DST4418_NAME1"_nand.bin
+cp ../../temporary/2ndboot/"$SEC4418_NAME"_SPI.bin   ./"$DST4418_NAME1"_spi.bin
+cp ../../temporary/2ndboot/"$SEC4418_NAME"_SPI.bin   ./"$DST4418_NAME2"_spi.bin
+cp ../../temporary/2ndboot/"$SEC4418_NAME"_SPI.bin   ./"$DST4418_NAME3"_spi.bin
+
+cp ../../temporary/2ndboot/"$SEC4330_NAME"_SDMMC.bin ./"$DST4330_NAME1"_sdmmc.bin
+cp ../../temporary/2ndboot/"$SEC4330_NAME"_USB.bin   ./"$DST4330_NAME1"_usb.bin
+cp ../../temporary/2ndboot/"$SEC4330_NAME"_SPI.bin   ./"$DST4330_NAME1"_spi.bin
--- boot/release/nsih/copy2release_nsih.sh
+++ boot/release/nsih/copy2release_nsih.sh
@@ -2,11 +2,12 @@
 
 SECBOOT_SDMMC=S5P4418_NSIH_V06_sdmmc_800_16x2_1cs_1GB.txt
 SECBOOT_USB=S5P4418_NSIH_V06_usb_800_16x2_1cs_1GB.txt
+SECBOOT_SPI=S5P4418_NSIH_V06_spi_800_16x2_1cs_1GB.txt
 
 DST_NAME1=nsih_svt
-DST_NAME2=nsih_dronel
-DST_NAME3=nsih_drone
-DST_NAME4=nsih_lepus
+DST_NAME2=nsih_drone
+DST_NAME3=nsih_lepus
+DST_NAME4=nsih_avb_ref
 
 cp ../../temporary/nsih/"$SECBOOT_SDMMC" ./"$DST_NAME1"_sdmmc.txt
 cp ../../temporary/nsih/"$SECBOOT_SDMMC" ./"$DST_NAME2"_sdmmc.txt
@@ -17,3 +18,8 @@ cp ../../temporary/nsih/"$SECBOOT_USB" ./"$DST_NAME1"_usb.txt
 cp ../../temporary/nsih/"$SECBOOT_USB" ./"$DST_NAME2"_usb.txt
 cp ../../temporary/nsih/"$SECBOOT_USB" ./"$DST_NAME3"_usb.txt
 cp ../../temporary/nsih/"$SECBOOT_USB" ./"$DST_NAME4"_usb.txt
+
+cp ../../temporary/nsih/"$SECBOOT_SPI" ./"$DST_NAME1"_spi.txt
+cp ../../temporary/nsih/"$SECBOOT_SPI" ./"$DST_NAME2"_spi.txt
+cp ../../temporary/nsih/"$SECBOOT_SPI" ./"$DST_NAME3"_spi.txt
+cp ../../temporary/nsih/"$SECBOOT_SPI" ./"$DST_NAME4"_spi.txt

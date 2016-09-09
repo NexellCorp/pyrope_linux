010d014d4d3fb5ca28bed8a3f14aa962959b59d6
SeongO Park <ray@nexell.co.kr>
2016-09-09 19:55:10 +0900

    apps: vd_cinema: add script
    Former-commit-id: 39171ec4ac6a8a042c34b791fd42a7a89301a0eb

--- apps/vd_cinema/push_sap.sh
+++ apps/vd_cinema/push_sap.sh
@@ -1,2 +1,10 @@
-adb push bin/ /system/bin/
-adb push lib/ /system/lib/
+adb shell mkdir -p /mnt/mmc/bin
+adb shell mkdir -p /mnt/mmc/lib
+
+adb push script/run_adb.sh /mnt/mmc/
+
+adb push lib/libnxcinema_linux.so /mnt/mmc/lib/
+adb push apps/sap_marriage/sap_marriage /mnt/mmc/bin/
+
+adb shell mkdir -p /mnt/mmc/bin/ruby
+adb push apps/sap_marriage/ruby/ /mnt/mmc/bin/ruby/

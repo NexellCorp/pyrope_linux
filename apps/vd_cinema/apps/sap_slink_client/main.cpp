97a4007b3dd41453689c289749d4c177ee28e314
SeongO Park <ray@nexell.co.kr>
2016-09-09 21:01:49 +0900

    app: vd_cinema: add some scripts.
    Former-commit-id: f2160dfc495baf99809b556a22bcb061874014bf

--- apps/vd_cinema/apps/sap_slink_client/main.cpp
+++ apps/vd_cinema/apps/sap_slink_client/main.cpp
@@ -17,30 +17,33 @@
 //
 //------------------------------------------------------------------------------
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>	//	usleep
 #include <stdlib.h>	//	atoi
 
-#include "CNX_Uart.h"
-#include "NX_UartProtocol.h"
+#include <CNX_Uart.h>
+#include <NX_UartProtocol.h>
 #include <NX_Utils.h>
 
-int main( int argc, char *argv[] )
+int32_t main( int32_t argc, char *argv[] )
 {
 	const char *TestString = "1234567890abcdef";
 	CNX_Uart *hUart = new CNX_Uart();
 	int32_t written;
 	uint32_t frameNumber = 0;
+	int32_t port = 0;
 
 	int32_t count = 1;
 
 	if( argc>1 )
 	{
-		count = atoi(argv[1]);
+		port = atoi(argv[1]);
 	}
+printf("________________ %d\n", __LINE__);
 
-	if( !hUart->Init( 2 ) )
+	if( !hUart->Init( port ) )
 	{
 		printf("Uart Init Error!!!\n");
 		return -1;
@@ -48,22 +51,27 @@ int main( int argc, char *argv[] )
 
 	NX_UART_PACKET pkt;
 
+printf("________________ %d\n", __LINE__);
+
 	NX_InitPacket( &pkt );
 
 	pkt.command = 0x12344321;
 	pkt.payload = (void *)TestString;
 	pkt.payloadSize = strlen(TestString);
 
+printf("________________ %d\n", __LINE__);
 	int32_t sendBufSize = NX_GetSendBufferSize( &pkt );
-	uint8_t *sendBuffer = (uint8_t *)malloc(sendBufSize);
+	uint8_t *sendBuffer = (uint8_t *)malloc(4096);
 
 	int32_t sendSize;
+printf("________________ %d\n", __LINE__);
 
 	if( count == -1 )
 	{
 		count = 0x7fffffff;
 	}
 
+printf("________________ %d\n", __LINE__);
 	for( int32_t i=0 ; i < count ; i++ )
 	{
 		pkt.frameNumber = frameNumber;
--- apps/vd_cinema/push_sap.sh
+++ apps/vd_cinema/push_sap.sh
@@ -3,8 +3,16 @@ adb shell mkdir -p /mnt/mmc/lib
 
 adb push script/run_adb.sh /mnt/mmc/
 
+# Digital Cinema Control Library.
+adb push lib/libnxcinema_linux.so /usr/lib/
 adb push lib/libnxcinema_linux.so /mnt/mmc/lib/
+
+# Marriage Test Application
 adb push apps/sap_marriage/sap_marriage /mnt/mmc/bin/
 
+# Secure Link Test Application
+adb push apps/sap_slink_client/sap_slink_client /mnt/mmc/bin/
+
+# Ruby Script for Digital Certificate
 adb shell mkdir -p /mnt/mmc/bin/ruby
 adb push apps/sap_marriage/ruby/ /mnt/mmc/bin/ruby/

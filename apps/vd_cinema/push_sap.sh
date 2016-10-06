36a39fd9ea56d69c66d3a2c72bd230e2c35d6b19
Sungwon Jo <doriya@nexell.co.kr>
2016-10-06 23:07:17 +0900

    vd_cinema: script: add permission handling.
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>
    
    Former-commit-id: c74bda91f75e27851b1862bc2146952cbcaf3e00

--- apps/vd_cinema/push_sap.sh
+++ apps/vd_cinema/push_sap.sh
@@ -1,14 +1,21 @@
 adb shell mkdir -p /mnt/mmc/bin
-adb shell mkdir -p /mnt/mmc/lib
+adb shell chmod 755 /mnt/mmc/bin
 
-adb push script/run_adb.sh /mnt/mmc/
+adb shell mkdir -p /mnt/mmc/lib
+adb shell chmod 755 /mnt/mmc/lib
+
+#adb push script/run_adb.sh /mnt/mmc/
 
 # Digital Cinema Control Library.
 adb push lib/libnxcinema_linux.so /mnt/mmc/lib/
+adb shell chmod 755 /mnt/mmc/lib/*.so
 
 # Secure Link Test Application
 adb push apps/sap_slink_client/sap_slink_client /mnt/mmc/bin/
+adb shell chmod 755 /mnt/mmc/bin/sap_slink_client
 
 # Ruby Script for Digital Certificate
 adb shell mkdir -p /mnt/mmc/bin/cert
 adb push script/cert/ /mnt/mmc/bin/cert/
+adb shell chmod 755 /mnt/mmc/bin/cert
+adb shell chmod 644 /mnt/mmc/bin/cert/*.*

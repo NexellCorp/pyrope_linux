e397406f75becb5ee024a1b02d9d0171043f4537
Sungwon Jo <doriya@nexell.co.kr>
2016-10-10 11:24:00 +0900

    vd_cinema: script: add adb serial id.
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>
    
    Former-commit-id: 5e335e3b1e6e366d5be83ec8af8120b609c8d258

--- apps/vd_cinema/push_nap.sh
+++ apps/vd_cinema/push_nap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # N.AP Start Script
-adb push script/nap_script.sh /system/bin/
+adb -s NAP push script/nap_script.sh /system/bin/
 
 # Digital Cinema Control Library.
-adb push lib/libnxcinema.so /system/lib/
+adb -s NAP push lib/libnxcinema.so /system/lib/
 
 # Test Application
-adb push bin/ /system/bin/
+adb -s NAP push bin/ /system/bin/
--- apps/vd_cinema/push_sap.sh
+++ apps/vd_cinema/push_sap.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # Make Directory
-adb shell mkdir -p /mnt/mmc/bin
-adb shell chmod 755 /mnt/mmc/bin
+adb -s SAP shell mkdir -p /mnt/mmc/bin
+adb -s SAP shell chmod 755 /mnt/mmc/bin
 
-adb shell mkdir -p /mnt/mmc/lib
-adb shell chmod 755 /mnt/mmc/lib
+adb -s SAP shell mkdir -p /mnt/mmc/lib
+adb -s SAP shell chmod 755 /mnt/mmc/lib
 
 # S.AP Start Script
-adb push script/sap_script.sh /mnt/mmc/
-adb shell chmod 755 /mnt/mmc/sap_script.sh
+adb -s SAP push script/sap_script.sh /mnt/mmc/
+adb -s SAP shell chmod 755 /mnt/mmc/sap_script.sh
 
 # Digital Cinema Control Library.
-adb push lib/libnxcinema_linux.so /mnt/mmc/lib/
-adb shell chmod 755 /mnt/mmc/lib/*.so
+adb -s SAP push lib/libnxcinema_linux.so /mnt/mmc/lib/
+adb -s SAP shell chmod 755 /mnt/mmc/lib/*.so
 
 # Secure Link Test Application
-adb push apps/sap_slink_client/sap_slink_client /mnt/mmc/bin/
-adb shell chmod 755 /mnt/mmc/bin/sap_slink_client
+adb -s SAP push apps/sap_slink_client/sap_slink_client /mnt/mmc/bin/
+adb -s SAP shell chmod 755 /mnt/mmc/bin/sap_slink_client
 
 # Ruby Script for Digital Certificate
-adb shell mkdir -p /mnt/mmc/bin/cert
-adb push script/cert/ /mnt/mmc/bin/cert/
-adb shell chmod 755 /mnt/mmc/bin/cert
-adb shell chmod 644 /mnt/mmc/bin/cert/*.*
+adb -s SAP shell mkdir -p /mnt/mmc/bin/cert
+adb -s SAP push script/cert/ /mnt/mmc/bin/cert/
+adb -s SAP shell chmod 755 /mnt/mmc/bin/cert
+adb -s SAP shell chmod 644 /mnt/mmc/bin/cert/*.*

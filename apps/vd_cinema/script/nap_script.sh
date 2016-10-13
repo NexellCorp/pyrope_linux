5dc51a0edf1338d185d1cfd886bad794f6cb2b2b
Sungwon Jo <doriya@nexell.co.kr>
2016-10-13 20:59:38 +0900

    vd_cinema: script: modify nap_script.sh  -. add default gateway and dns.  -. add ntp daemon.
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>
    
    Former-commit-id: 14970fe1af708eb28ec5e2dcfc050c0947e9c58e

--- apps/vd_cinema/script/nap_script.sh
+++ apps/vd_cinema/script/nap_script.sh
@@ -5,7 +5,12 @@ ifconfig eth0 192.168.10.222 netmask 255.255.255.0
 ndc network create 100 
 ndc network interface add 100 eth0
 ndc network route add 100 eth0 192.168.10.0/24
+ndc network route add 100 eth0 0.0.0.0/0 192.168.10.1
+ndc resolver setnetdns 100 localdomain 8.8.8.8
 ndc network default set 100
+
+# NTP daemon
+busybox ntpd -p 115.90.134.38 -l
 
 # Start Application
 /system/bin/nap_server &

ec81f23e22109c9aeb687cf6887ced63a6a10aa1
SeongO Park <ray@nexell.co.kr>
2016-11-23 13:30:24 +0900

    3d: graphic tools: example: add NR function examples.
    Signed-off-by: SeongO Park <ray@nexell.co.kr>
    
    Former-commit-id: d14bbfafc779c741df770175400da5c3738417a9

---
+++ library/src/libnxgraphictools/examples/nr_enc_test/Android.mk
@@ -0,0 +1,40 @@
+LOCAL_PATH := $(call my-dir)
+
+##############################################################################
+##
+##     Graphic Library Test Application
+##
+
+include $(CLEAR_VARS)
+NX_HW_TOP        := $(TOP)/hardware/samsung_slsi/slsiap/
+NX_HW_INCLUDE    := $(NX_HW_TOP)/include
+NX_LINUX_INCLUDE := $(TOP)/linux/platform/s5p4418/library/include
+
+LOCAL_SHARED_LIBRARIES :=	\
+	liblog \
+	libcutils \
+	libnxgraphictools \
+	libion-nexell \
+	libion \
+	libnx_vpu
+
+LOCAL_STATIC_LIBRARIES :=	\
+	libnxmalloc
+
+LOCAL_C_INCLUDES :=	$(TOP)/system/core/include/ion \
+					$(NX_HW_INCLUDE) \
+					$(LOCAL_PATH)/src \
+					$(NX_LINUX_INCLUDE) \
+					$(LOCAL_PATH)/include/khronos
+
+LOCAL_CFLAGS := 
+
+LOCAL_SRC_FILES := \
+	main.cpp
+
+LOCAL_MODULE := nr_enc_test
+LOCAL_MODULE_PATH := $(LOCAL_PATH)
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)

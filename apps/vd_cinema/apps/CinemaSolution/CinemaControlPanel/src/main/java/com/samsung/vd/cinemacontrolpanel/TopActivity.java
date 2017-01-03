52a445db661b02ee5f306a1ef323e85a7d33839e
Sungwon Jo <doriya@nexell.co.kr>
2017-01-03 22:03:19 +0900

    vd_cinema: app: change top menu for DCI.  -. remove summary page.  -. add image quality function.
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>
    
    Former-commit-id: 240704285cf14a30c0170651ef43584d924f4127

--- apps/vd_cinema/apps/CinemaSolution/CinemaControlPanel/src/main/java/com/samsung/vd/cinemacontrolpanel/TopActivity.java
+++ apps/vd_cinema/apps/CinemaSolution/CinemaControlPanel/src/main/java/com/samsung/vd/cinemacontrolpanel/TopActivity.java
@@ -1,9 +1,12 @@
 package com.samsung.vd.cinemacontrolpanel;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
@@ -11,19 +14,71 @@ import android.text.Layout;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import com.samsung.vd.baseutils.VdStatusBar;
 import com.samsung.vd.baseutils.VdTitleBar;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by doriya on 8/17/16.
  */
 public class TopActivity extends AppCompatActivity {
     private final String VD_DTAG = "DiagnosticsActivity";
+
+    private byte[]  mCabinet;
+
+    private Spinner mSpinnerImageQuality;
+    private Button mBtnImageQuality;
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if( intent.getAction().equals(Intent.ACTION_MEDIA_MOUNTED) ) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                String[] resultQuality = CheckFileInUsb(LedQualityInfo.PATH, LedQualityInfo.NAME);
+                String[] resultGamma = CheckFileInUsb(LedGammaInfo.PATH, LedGammaInfo.PATTERN_NAME);
+
+                if( (resultQuality != null && resultQuality.length != 0) || (resultGamma != null && resultGamma.length != 0) ) {
+                    mBtnImageQuality.setEnabled(true);
+                }
+            }
+            if( intent.getAction().equals(Intent.ACTION_MEDIA_EJECT) ) {
+                mBtnImageQuality.setEnabled(false);
+            }
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction( Intent.ACTION_MEDIA_MOUNTED );
+        filter.addAction( Intent.ACTION_MEDIA_EJECT );
+        filter.addDataScheme("file");
+
+        registerReceiver( mBroadcastReceiver, filter );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver( mBroadcastReceiver );
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +113,11 @@ public class TopActivity extends AppCompatActivity {
         new VdStatusBar( getApplicationContext(), (LinearLayout)findViewById( R.id.statusBarLayoutTop) );
 
         //
+        //  Cinema System Information
+        //
+        mCabinet = ((CinemaInfo)getApplicationContext()).GetCabinet();
+
+        //
         //  Alert Message
         //
         int configCabinetNum = Integer.parseInt(((CinemaInfo)getApplicationContext()).GetValue(CinemaInfo.KEY_CABINET_NUM));
@@ -66,6 +126,19 @@ public class TopActivity extends AppCompatActivity {
             String strMessage = String.format(Locale.US, "Please Check Cabinet Number. ( setting: %d, detect: %d )", configCabinetNum, detectCabientNum);
             CinemaAlert.Show( TopActivity.this, "Alert",  strMessage );
         }
+
+        mSpinnerImageQuality = (Spinner)findViewById(R.id.spinnerImageQuality);
+        mSpinnerImageQuality.setAdapter( new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, new String[] { "1", "2", "3", "4"}));
+
+        mBtnImageQuality = (Button)findViewById(R.id.btnImageQuality);
+        mBtnImageQuality.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AsyncTaskImageQuality().execute();
+            }
+        });
+
+        UpdateImageQuality();
 
         //
         //
@@ -129,6 +202,106 @@ public class TopActivity extends AppCompatActivity {
             }
         }
     };
+
+    private void UpdateImageQuality() {
+        String[] resultQuality = CheckFileInUsb(LedQualityInfo.PATH, LedQualityInfo.NAME);
+        String[] resultGamma = CheckFileInUsb(LedGammaInfo.PATH, LedGammaInfo.PATTERN_NAME);
+        if( (resultQuality == null || resultQuality.length == 0) && (resultGamma == null || resultGamma.length == 0) )
+            return;
+
+        mBtnImageQuality.setEnabled(true);
+    }
+
+    //
+    //
+    //
+    private String[] CheckFileInUsb( String topdir, String regularExpression ) {
+        String[] result = new String[0];
+        for( int i = 0; i < 10; i++ ) {
+            File topfolder = new File( String.format(Locale.US, "/storage/usbdisk%d/%s", i, topdir) );
+            File[] toplist = topfolder.listFiles();
+            if( toplist == null || toplist.length == 0 )
+                continue;
+
+            Pattern pattern = Pattern.compile( regularExpression );
+            for( File file : toplist ) {
+                if( !file.isFile() )
+                    continue;
+
+                Matcher matcher = pattern.matcher(file.getName());
+                if( matcher.matches() ) {
+                    String[] temp = Arrays.copyOf( result, result.length + 1);
+                    temp[result.length] = file.getAbsolutePath();
+                    result = temp;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private class AsyncTaskImageQuality extends AsyncTask<Void, Void, Void> {
+        private int mIndexQuality = mSpinnerImageQuality.getSelectedItemPosition();
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            NxCinemaCtrl ctrl = NxCinemaCtrl.GetInstance();
+
+            boolean bValidPort0 = false, bValidPort1 = false;
+            for( byte id : mCabinet ) {
+                if( 0 == ((id >> 7) & 0x01) ) bValidPort0 = true;
+                if( 1 == ((id >> 7) & 0x01) ) bValidPort1 = true;
+            }
+
+            String[] result;
+            result = CheckFileInUsb(LedQualityInfo.PATH, LedQualityInfo.NAME);
+            for( String file : result ) {
+                LedQualityInfo info = new LedQualityInfo();
+                if( info.Parse( file ) ) {
+                    for( int i = 0; i < info.GetRegister().length; i++ ) {
+                        byte[] reg = ctrl.IntToByteArray(info.GetRegister()[i], NxCinemaCtrl.FORMAT_INT8);
+                        byte[] data = ctrl.IntToByteArray(info.GetData(mIndexQuality)[i], NxCinemaCtrl.FORMAT_INT16);
+                        byte[] inData = ctrl.AppendByteArray(reg, data);
+
+                        if( bValidPort0 ) ctrl.Send( 0x09, NxCinemaCtrl.CMD_TCON_QUALITY, inData);
+                        if( bValidPort1 ) ctrl.Send( 0x89, NxCinemaCtrl.CMD_TCON_QUALITY, inData);
+                    }
+                }
+            }
+
+            result = CheckFileInUsb(LedGammaInfo.PATH, LedGammaInfo.PATTERN_NAME);
+            for( String file : result ) {
+                LedGammaInfo info = new LedGammaInfo();
+                if( info.Parse( file ) ) {
+                    int cmd;
+                    if( info.GetType() == LedGammaInfo.TYPE_TARGET )
+                        cmd = NxCinemaCtrl.CMD_TCON_TGAM_R;
+                    else
+                        cmd = NxCinemaCtrl.CMD_TCON_DGAM_R;
+
+                    byte[] table = ctrl.IntToByteArray(info.GetTable(), NxCinemaCtrl.FORMAT_INT8);
+                    byte[] data = ctrl.IntArrayToByteArray(info.GetData(), NxCinemaCtrl.FORMAT_INT24);
+                    byte[] inData = ctrl.AppendByteArray(table, data);
+
+                    if( bValidPort0 ) ctrl.Send( 0x09, cmd + info.GetChannel(), inData );
+                    if( bValidPort1 ) ctrl.Send( 0x89, cmd + info.GetChannel(), inData );
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            CinemaLoading.Show( TopActivity.this );
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            CinemaLoading.Hide();
+        }
+    }
 
     //
     //  For ScreenSaver

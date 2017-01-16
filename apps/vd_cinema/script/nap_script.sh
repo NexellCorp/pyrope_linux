cc6dc23e040179bdf6de4bc4915539cb5a35eac1
Sungwon Jo <doriya@nexell.co.kr>
2017-01-16 11:50:34 +0900

    vd_cinema: script: change ntp server poll.
    Change-Id: I3bb555be5b1ca844533feda96aec03a897c6cdf2
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>
    
    Former-commit-id: 088557f49006dbfcf17077ba403e88e1bed4ad8a

--- apps/vd_cinema/script/nap_script.sh
+++ apps/vd_cinema/script/nap_script.sh
@@ -4,7 +4,7 @@
 /system/bin/nap_network.sh start
 
 # NTP daemon
-busybox ntpd -p 115.90.134.38 -l
+busybox ntpd -p 216.239.35.0 -p 216.239.35.4 -p 216.239.35.8 -p 216.239.35.12 -l
 
 # Start Application
 /system/bin/nap_server &

2c84a53eb65b7eb5a3c155896fef979619834b21
Sungwon Jo <doriya@nexell.co.kr>
2018-07-05 12:20:11 +0900

    vd_cinema: script: change package sciprt in Diagnostics.  -. modified to be packaging even if no binary files.
    Change-Id: I6f10b03e8a216400106b9b8e03a7c3078d253fbb
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>
    
    Former-commit-id: d3c28c57c323ba1bc48e28c5113983a43ce3a270

--- apps/vd_cinema/apps/CinemaDiagnostics/package.sh
+++ apps/vd_cinema/apps/CinemaDiagnostics/package.sh
@@ -3,27 +3,20 @@ NX_DIR_WORKING=$(pwd)
 NX_DIR_SCRIPT=$(cd "$(dirname "$0")" && pwd)
 NX_DIR_RESULT=$NX_DIR_SCRIPT/result
 
-if ! [ -f $NX_DIR_SCRIPT/NxCinemaDiagnostics/NxCinemaDiagnostics ]; then
-	echo "Plase build \"NxCinemaDiagnostics\"."
-	exit 1
+if [ -f $NX_DIR_SCRIPT/NxCinemaDiagnostics/NxCinemaDiagnostics ]; then
+	cmp -s $NX_DIR_SCRIPT/NxCinemaDiagnostics/NxCinemaDiagnostics $NX_DIR_RESULT/bin/NxCinemaDiagnostics
+	if [ $? -ne 0 ]; then
+		echo ">>> Update NxCinemaDiagnostics"
+		cp -av $NX_DIR_SCRIPT/NxCinemaDiagnostics/NxCinemaDiagnostics $NX_DIR_RESULT/bin/NxCinemaDiagnostics
+	fi
 fi
 
-
-if ! [ -f $NX_DIR_SCRIPT/NxCinemaDiagnosticsPanel/Release/NxCinemaDiagnosticsPanel.exe ]; then
-	echo "Plase build \"NxCinemaDiagnosticsPanel\"."
-	exit 1
-fi
-
-cmp -s $NX_DIR_SCRIPT/NxCinemaDiagnostics/NxCinemaDiagnostics $NX_DIR_RESULT/bin/NxCinemaDiagnostics
-if [ $? -ne 0 ]; then
-	echo ">>> Update NxCinemaDiagnostics"
-	cp -av $NX_DIR_SCRIPT/NxCinemaDiagnostics/NxCinemaDiagnostics $NX_DIR_RESULT/bin/NxCinemaDiagnostics
-fi
-
-cmp -s $NX_DIR_SCRIPT/NxCinemaDiagnosticsPanel/Release/NxCinemaDiagnosticsPanel.exe $NX_DIR_RESULT/NxCinemaDiagnosticsPanel.exe
-if [ $? -ne 0 ]; then
-	echo ">>> Update NxCinemaDiagnosticsPanel"
-	cp -av $NX_DIR_SCRIPT/NxCinemaDiagnosticsPanel/Release/NxCinemaDiagnosticsPanel.exe $NX_DIR_RESULT/NxCinemaDiagnosticsPanel.exe
+if [ -f $NX_DIR_SCRIPT/NxCinemaDiagnosticsPanel/Release/NxCinemaDiagnosticsPanel.exe ]; then
+	cmp -s $NX_DIR_SCRIPT/NxCinemaDiagnosticsPanel/Release/NxCinemaDiagnosticsPanel.exe $NX_DIR_RESULT/NxCinemaDiagnosticsPanel.exe
+	if [ $? -ne 0 ]; then
+		echo ">>> Update NxCinemaDiagnosticsPanel"
+		cp -av $NX_DIR_SCRIPT/NxCinemaDiagnosticsPanel/Release/NxCinemaDiagnosticsPanel.exe $NX_DIR_RESULT/NxCinemaDiagnosticsPanel.exe
+	fi
 fi
 
 cd $NX_DIR_SCRIPT/result

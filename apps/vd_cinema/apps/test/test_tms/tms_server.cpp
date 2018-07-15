dcb24daf2cb0958bea2d58b3074db0a5eb779dfd
Sungwon Jo <doriya@nexell.co.kr>
2018-07-16 08:52:35 +0900

    vd_cinema: app: add tms test code.
    Change-Id: Ida7e90f329bcfde292f47b5459f73e966780d367
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>
    
    Former-commit-id: 20917971ff6cabcdb9a683e1143cc29a0ea740df

--- apps/vd_cinema/apps/test/test_tms/tms_server.cpp
+++ apps/vd_cinema/apps/test/test_tms/tms_server.cpp
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //
-//	Copyright (C) 2016 Nexell Co. All Rights Reserved
+//	Copyright (C) 2018 Nexell Co. All Rights Reserved
 //	Nexell Co. Proprietary & Confidential
 //
 //	NEXELL INFORMS THAT THIS CODE AND INFORMATION IS PROVIDED "AS IS" BASE
@@ -17,127 +17,64 @@
 //
 //------------------------------------------------------------------------------
 
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
+#include <signal.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/un.h>
-#include <errno.h>
 
-#include <SockUtils.h>
-#include <tms_protocol.h>
-#include <NX_Utils.h>
-
-#define	TMS_SERVER_FILE "/data/local/tmp/ipc_server"
-#define	MAX_PAYLOAD_SIZE	4096
+#include <NX_TMSServer.h>
 
 //------------------------------------------------------------------------------
-static int32_t ReadData(int32_t fd, uint8_t *pBuf, int32_t size)
+static void signal_handler( int32_t signal )
 {
-	int32_t readSize, totalSize=0;
-	do
-	{
-		readSize = read( fd, pBuf, size );
-		if( readSize < 0 )
-			return -1;
+	printf("Aborted by signal %s (%d)..\n", (char*)strsignal(signal), signal);
 
-		size -= readSize;
-		pBuf += readSize;
-		totalSize += readSize;
-	}while(size > 0);
-	return totalSize;
+	switch( signal )
+	{
+		case SIGINT :
+			printf("SIGINT..\n"); 	break;
+		case SIGTERM :
+			printf("SIGTERM..\n");	break;
+		case SIGABRT :
+			printf("SIGABRT..\n");	break;
+		default :
+			break;
+	}
+
+	NX_TMSServerStop();
+	exit(EXIT_FAILURE);
 }
 
 //------------------------------------------------------------------------------
-int main( void )
+static void register_signal( void )
 {
-	int32_t svrSock;
-	int32_t readLen;
-	//	client socket
-	int32_t clntSock;
-	struct sockaddr_un clntAddr;
-	int32_t clntAddrSize;
+	signal( SIGINT,  signal_handler );
+	signal( SIGTERM, signal_handler );
+	signal( SIGABRT, signal_handler );
+}
 
-	int32_t readSize;
+//------------------------------------------------------------------------------
+static void ProcessCallback( uint32_t iCmd, uint8_t *pInBuf, int32_t iInSize, uint8_t *pOutBuf, int32_t *iOutSize, void *pObj )
+{
+	(void*)pObj;
+	*pOutBuf  = 0x01;
+	*iOutSize = 1;
+}
 
-	uint8_t *pBuf = (uint8_t *)malloc(MAX_PAYLOAD_SIZE);
-	uint8_t *payload;
-	int32_t payloadSize;
-	int32_t len;
-	uint32_t key=0, cmd=0;
-	uint8_t tmp;
+//------------------------------------------------------------------------------
+int32_t main( int32_t argc, char *argv[] )
+{
+	register_signal();
+	NX_TMSServerProcessCallback( &ProcessCallback );
+	NX_TMSServerStart();
 
-	svrSock = LS_Open( TMS_SERVER_FILE );
-	if( svrSock < 0 ){
-		printf("LS_Open Failed !!!\n");
-		return -1;
-	}
-
-	while(1)
+	while( 1 )
 	{
-		if( -1 == listen(svrSock, 5) )
-		{
-			printf( "Error : listen (err = %d)\n", errno);
-			exit( 1);
-		}
-
-		clntAddrSize = sizeof( clntAddr );
-		clntSock = accept( svrSock, (struct sockaddr*)&clntAddr, &clntAddrSize );
-
-		if ( -1 == clntSock)
-		{
-			printf( "Error : accept (err = %d)\n", errno);
-			exit( 1);
-		}
-
-		//
-		//	Find Key Code
-		//
-		do{
-			readSize = ReadData( clntSock, &tmp, 1 );
-			if( readSize != 1 )
-			{
-				printf("Read Error!!!\n");
-				goto ERROR;
-			}
-			key = (key<<8) | tmp;
-			if( key == TMS_KEY_VALUE )
-			{
-				break;
-			}
-		}while(1);
-
-		//	Read Length
-		readSize = ReadData(clntSock, pBuf, 2);
-		if( readSize <= 0 ){
-			printf("Error : Read Length\n");
-			goto ERROR;
-		}
-		len = TMS_GET_LENGTH(pBuf[0], pBuf[1]);
-
-		//	Read Data
-		readLen = read ( clntSock, pBuf+2, len );
-		if( readLen <=0 )
-		{
-			printf("Error : Read Data\n");
-			goto ERROR;
-		}
-
-		// 	Command
-		cmd = TMS_GET_COMMAND(pBuf[2], pBuf[3]);
-
-		//	Payload
-		payload = pBuf + 4;
-		payloadSize = len - 2;
-
-		printf( "Receive Data: Length(%d), Command(0x%08x), PayloadSize(%d)\n",
-				len, cmd, payloadSize );
-
-ERROR:
-		close( clntSock );
+		usleep(1000000);
 	}
+
+	NX_TMSServerStop();
 	return 0;
 }

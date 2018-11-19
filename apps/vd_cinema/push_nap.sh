501b121516babacc3d25501a83f30907e073526b
Sungwon Jo <doriya@nexell.co.kr>
2018-11-19 11:23:50 +0900

    vd_cinema: scripts: update to push nap birary.  -. add to push in network adb. ( default )
    Change-Id: Ic3668504db98ab12f695d4a0ccd32549ebf98eb5
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>
    Reviewed-on: https://review.gerrithub.io/437075
    Reviewed-by: <ray@nexell.co.kr>
    Tested-by: <ray@nexell.co.kr>

--- apps/vd_cinema/push_nap.sh
+++ apps/vd_cinema/push_nap.sh
@@ -1,15 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+
+NX_DEVICE=
+NX_ADB_DEVICES=$(adb devices)
+NX_ADB_NETWORK=$(echo $NX_ADB_DEVICES | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}:[0-9]\{1,4\}")
+
+if [[ -n $NX_ADB_NETWORK ]]; then
+	NX_DEVICE=$NX_ADB_NETWORK
+elif [[ $NX_ADB_DEVICES =~ "NAP" ]]; then
+	NX_DEVICE="NAP"
+fi
+
+if [[ -z $NX_DEVICE ]]; then
+	echo "adb device not connected."
+	exit -1;
+fi
+
+echo ">> ADB Target Device : $NX_DEVICE"
 
 # Change adb root permission
-adb -s NAP root
+NX_ADB_ROOT=$(adb -s $NX_DEVICE root)
+if [[ $NX_ADB_ROOT != "adbd is already running as root" ]]; then
+	echo "change root permission.."
+	sleep 4;
+fi
 
 # N.AP Start Script
-adb -s NAP push script/nap_network /system/bin/
-adb -s NAP push script/nap_network.sh /system/bin/
-adb -s NAP push script/nap_script.sh /system/bin/
+adb -s $NX_DEVICE push script/nap_network /system/bin/
+adb -s $NX_DEVICE push script/nap_network.sh /system/bin/
+adb -s $NX_DEVICE push script/nap_script.sh /system/bin/
 
 # Digital Cinema Control Library
-adb -s NAP push lib/libnxcinema.so /system/lib/
+adb -s $NX_DEVICE push lib/libnxcinema.so /system/lib/
 
 # Test Application
-adb -s NAP push bin/ /system/bin/
+adb -s $NX_DEVICE push bin/ /system/bin/
+
